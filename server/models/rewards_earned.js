909c01e6c2e8672ee7f1cf6336eafbe356a9e8ea
Chitosama <lilchitodude51@gmail.com>
2023-03-13 18:51:37 -0700

    fixed file extensions

--- server/models/rewards_earned.js
+++ server/models/rewards_earned.js
@@ -1,43 +1,4 @@
 const { Model, DataTypes } = require("sequelize");
-// const sequelize = require("../config");
-const sequelize = require("../config");
-
-class Reward_earned extends Model {}
-
-Reward_earned.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    user_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: "user",
-        key: "id",
-      },
-    },
-    reward: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: "reward_list",
-        key: "id",
-      },
-    },
-  },
-  {
-    sequelize,
-    timestamps: true,
-    freezeTableName: true,
-    underscored: true,
-    modelName: "reward_earned",
-  }
-);
-
-module.exports = Reward_earned;
-const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config");
 
 class Reward_earned extends Model {}
