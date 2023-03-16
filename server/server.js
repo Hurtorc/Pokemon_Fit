3156a15299039df928793e528a80fd270d81c281
Rj Saini <rsaini88@gmail.com>
2023-03-15 21:57:56 -0700

    updated dependencies

--- server/server.js
+++ server/server.js
@@ -1,13 +1,43 @@
-const express = require('express');
+const express = require("express");
+const session = require("express-session");
+const sequelize = require("./config/connection");
+require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
-app.use(express.static('../client/dist'));
+app.use(express.static("../client/dist"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require('./routes/htmlRoutes')(app);
+require("./routes/htmlRoutes")(app);
 
-app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
+//const routes = require('./controllers');
 
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const sess = {
+  secret: "Super secret secret",
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: "strict",
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+app.use(express.static("../client/dist"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Now listening on port: ${PORT}`);
+  sequelize.sync({ force: false });
+});
