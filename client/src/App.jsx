88041098b7d537a1eb95345fe8594b3571ab4dc6
Rj Saini <rsaini88@gmail.com>
2023-03-15 22:06:33 -0700

    fixed shit added shit modified shit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,22 @@
-
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Routes, Route}
-    from 'react-router-dom';
-import Welcome from './pages/Welcome';
-import Page3 from './pages/Page 3';
-import Dashboard from './pages/Dashboard';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Welcome from "./pages/Welcome";
+import Page3 from "./pages/Page 3";
+import Dashboard from "./pages/Dashboard";
+import SignInSide from "./pages/SignInSide";
 
 function App() {
-return (
+  return (
     <Router>
-    <Routes>
-        <Route exact path='/' element={<Welcome />} />
-        <Route path='/page3' element={<Page3/>} />
-        <Route path='/dashboard' element={<Dashboard/>} />
-    </Routes>
+      <Routes>
+        <Route exact path="/" element={<Welcome />} />
+        <Route path="/page3" element={<Page3 />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/sign" element={<SignInSide />} />
+      </Routes>
     </Router>
-);
+  );
 }
-  
+
 export default App;
