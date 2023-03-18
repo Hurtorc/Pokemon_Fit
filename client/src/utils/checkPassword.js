af2f901c029168ab087392a2485730f6a3edf3a6
Rj Saini <rsaini88@gmail.com>
2023-03-17 22:42:01 -0700

    new branch test

--- client/src/utils/checkPassword.js
+++
@@ -1,9 +0,0 @@
-// create a function that checks a password for a minimum length of 4 characters
-
-function checkPassword(password) {
-  if (password.length < 4) {
-    return false;
-  } else {
-    return true;
-  }
-}
--- server/schema/schema.sql
+++
@@ -1,5 +0,0 @@
--- DROP DATABASE
-DROP DATABASE IF EXISTS pokemon_fit_db;
-
--- CREATE DATABASE
-CREATE DATABASE pokemon_fit_db;
